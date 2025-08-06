44f16c688e04899db70c72fbfde0ffd0e92b27cc
hdp1904 <33026524+hdp1904@users.noreply.github.com>
2025-08-06 10:44:40 -0700

    Update KeepingComponentsPure.jsx

--- advance/react/src/RenderingTheUI/KeepingComponentsPure.jsx
+++ advance/react/src/RenderingTheUI/KeepingComponentsPure.jsx
@@ -4,7 +4,7 @@ export default function KeepingComponentsPure() {
     return (
         <div>
             <div>
-           
+                KeepingComponentsPure
             </div>
             <Person name="Jack" age={11} profession="neuron surgeon" />
         </div>
