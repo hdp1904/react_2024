c30a744baca5189e2932be7cfda0b498ff6f15fa
hdp1904 <33026524+hdp1904@users.noreply.github.com>
2025-08-16 10:42:40 -0700

    Update store.js

--- advance/react/src/redux/store.js
+++ advance/react/src/redux/store.js
@@ -6,7 +6,7 @@ import storage from "redux-persist/lib/storage"; // defaults to localStorage for
 
 const persistConfig = {
   key: "root",
-  storage,
+
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
