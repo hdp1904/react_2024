a0539399082be0a034b515a9f752969bf4a69d82
hdp1904 <33026524+hdp1904@users.noreply.github.com>
2025-09-01 09:07:57 -0700

    Update useForceUpdate.jsx

--- advance/react/src/Day6/hooks/useForceUpdate.jsx
+++ advance/react/src/Day6/hooks/useForceUpdate.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 export default function useForceUpdate() {
-  const [bool, toggle] = useState(false);
+
   const rerender = () => {
     toggle((prev) => !prev);
   };
